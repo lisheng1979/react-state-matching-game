12d51a72859e48cf1b8794df4303077e56beebe3
lisheng1979 <skyeyes@263.net>
2020-06-15 23:58:55 -0700

    Completed module

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,28 @@ import Board from '../Board'
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props)
 
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex: null,
+      tiles: [],
+      toBeCleared: null
+    }
+  }
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel />
-        <Board />
-      }
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+          Turbo-Matcher
+        </header>
+          <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} />
+          <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+        }
+      </div>
+    );
 
   }
 }
