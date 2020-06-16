01df4ec1028a74375602a63be3d386562d6c93d8
lisheng1979 <skyeyes@263.net>
2020-06-16 00:24:18 -0700

    Completed module

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-
+import { createTiles } from '../../misc/utils'
 import './App.css';
 
 class App extends Component{
@@ -16,13 +16,21 @@ class App extends Component{
       toBeCleared: null
     }
   }
+  startGame = (numTiles) => {
+    this.setState((state) => {
+      playing: true,
+      previousTileIndex: null,
+      toBeCleared: null,
+      tiles: createTiles(state.numTiles)
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Turbo-Matcher
         </header>
-          <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} />
+          <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}/>
           <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
         }
       </div>
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Board.css';
-
+import Tile from '../Tile'
 const Board = (props) => {
 
   const gridConfig = {
@@ -8,9 +8,10 @@ const Board = (props) => {
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
   }
 
-
+  const tiles = props.tiles.map((tile) => (<Tile{...tile} />))
   return (
     <div className='Board' style={gridConfig}>
+    {tiles}
     </div>
   )
 }
