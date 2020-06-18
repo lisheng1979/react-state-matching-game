bd94f36b622eec37a8bcfd4d67f21f9d8c4b57be
lisheng1979 <skyeyes@263.net>
2020-06-18 00:16:26 -0700

    Completed module

---
+++ refactor/GameContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameContext=React.createContext({
+  numTiles: 36,
+  playing: false,
+  handleNumTileChange: ()=>{},
+  startPlaying: () =>{}
+
+})
+
+export default GameContext
